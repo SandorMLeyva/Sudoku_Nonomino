c7d9f695427833b0159b09d50152c1734e59ef42
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-05-21 17:53:55 -0400

    se agrego nonoToListColumn y Row para obtener todos los puntos que pertenecen a una misma fila o columna

--- main.hs
+++ main.hs
@@ -1,6 +1,6 @@
 data Point = Point {
-    x :: Integer,
-    y :: Integer,
+    x_p :: Integer,
+    y_p :: Integer,
     value :: Integer
 } deriving Show
 
@@ -20,16 +20,24 @@ data Nonomino = Nonomino {
 p = Point 0 1
 o = Point 0 2
 
-n = Nonomino (Point 0 1 0) (Point 0 1 2) (Point 0 1 2) (Point 0 1 4) (Point 0 1 5) (Point 0 1 7) (Point 0 1 0) (Point 0 1 0) (Point 0 1 0)
+n = Nonomino (Point 0 1 0) (Point 0 1 2) (Point 0 1 2) (Point 0 1 4) (Point 0 1 5) (Point 0 3 7) (Point 0 3 0) (Point 0 1 0) (Point 0 1 0)
+n1 = Nonomino (Point 0 2 1) (Point 1 1 3) (Point 0 1 4) (Point 0 2 0) (Point 0 1 5) (Point 0 1 7) (Point 0 1 0) (Point 0 1 0) (Point 0 1 0)
+n2 = Nonomino (Point 1 1 1) (Point 0 1 3) (Point 0 1 4) (Point 0 2 0) (Point 0 1 5) (Point 0 1 7) (Point 0 1 0) (Point 0 1 0) (Point 0 1 0)
 
+ll = [n,n1,n2]
 
 -- Convierte un nonomino en una lista de puntos
 nonoToList :: Nonomino -> [Point]
 nonoToList n = [p0 n,p1 n,p2 n,p3 n, p4 n,p5 n,p6 n,p7 n,p8 n]
 -- Dice si un nonomino es valido (si no tiene numero que se repitan)
-nonominoValido :: [Point] -> [Int] -> Bool
+--nonominoValido :: [Point] -> [Int] -> Bool
 nonominoValido [] _ = True
 nonominoValido ((Point _ _ value):nns) l = if value == 0 || value `elem` l  then nonominoValido nns [x | x <- l, x /= value || x == 0] else False
---
----- Comprueba si una fila es valida
---nonoFilaValida (nnl:nnls) i = nonoToList nnl
+
+-- Devuelve todos los elementos de una fila
+--nonoToListRow, nonoToListColumn :: [Nonomino] -> Int -> [Point]
+nonoToListRow [] _ = []
+nonoToListRow (nnl:nnls) i = [x | x <- nonoToList nnl, x_p(x) == i ] ++ nonoToListRow nnls i
+-- Devuelve todos los elementos de una columna
+nonoToListColumn [] _ = []
+nonoToListColumn (nnl:nnls) i = [x | x <- nonoToList nnl, y_p(x) == i ] ++ nonoToListRow nnls i
